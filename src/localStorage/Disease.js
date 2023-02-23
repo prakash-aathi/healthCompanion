8adf068ea6f91b088937eb9b49e5d45f9e28cc98
Prakash A <prakashaathi321@gmail.com>
2023-02-23 09:09:28 +0530

    data

--- src/localStorage/Disease.js
+++ src/localStorage/Disease.js
@@ -1,14 +1,213 @@
 export const DiseaseList = [
     {
         id: 1,
-        name: "Diabetes",
-        nutrients: ["iron","vitamin c"],
-        food: ["apple","orange"]
-    },
-    {
+        name: "Diabetes Type 1 ",
+        nutrients: ["Low fat","Carbodhydrates"],
+        food: ["oats","Barley","Bread"]
+        },
+        
+        {
         id: 2,
+        name: "Diabetes Type 2",
+        nutrients: ["High Fiber","Starch"],
+        food: ["Brown Rice","Beans"]
+        },
+        
+        {
+        id: 3,
+        name: "Malaria",
+        nutrients: ["Vitamin A","Vitamin C"],
+        food: ["Leafy Greens","Lemon","papaya"]
+        },
+        
+        {
+        id: 4,
         name: "Anemia",
-        nutrients: ["carbohydrates","vitamin D"],
-        food: ["betroot","orange"]
-    }
+        nutrients: ["Iron","Vitamin B12","Vitamin C"],
+        food: ["Beetroot","Dates","Spinach"]
+        },
+        
+        {
+        id: 5,
+        name: "Cholera",
+        nutrients: ["Fluid Rich Food"],
+        food: ["Apple","Cucumber"]
+        },
+        
+        {
+        id: 6,
+        name: "Cardiovascular Disease",
+        nutrients: ["Protein","Antioxidants","Fiber"],
+        food: ["Wholegrains","Salmon","Sprouts"]
+        },
+        
+        {
+        id: 7,
+        name: "Thyroid Ailments",
+        nutrients: ["Iodine","Fiber","Starch"],
+        food: ["Egg","Potato","Bread"]
+        },
+        
+        {
+        id: 8,
+        name: "Chronic Kidney Disease",
+        nutrients: ["Protein"],
+        food:["Banana stem","Banana Flower","Water"]
+        },
+        
+        {
+        id: 9,
+        name: "Chronic obstructive pulmonary disease - COPD",
+        nutrients: ["Magnesium","Vitamin D"],
+        food: ["Pumpkin Seeds","Spinach","Nuts"]
+        },
+        
+        {
+        id: 10,
+        name: "Pneumonia",
+        nutrients: ["High Protein","Antioxidant"],
+        food: ["Peanuts","Beans"]
+        },
+        
+        {
+        id: 11,
+        name: "Measles",
+        nutrients: ["Vitamin A","Vitamin C","Zinc"],
+        food: ["Milk","Egg","Greens"]
+        },
+        
+        {
+        id: 12,
+        name: "Asthma",
+        nutrients: ["Vitamin D","Magnesium","Beta Carotene"],
+        food: ["Carrot","Spinach","Milk"]
+        },
+        
+        {
+        id: 13,
+        name: "Tuberculosis",
+        nutrients: ["Vitamin D","Zinc","Iron"],
+        food: ["Shellfish","Guava","Greens","Dates"]
+        },
+        
+        {
+        id: 14,
+        name: "Hepatitis A,B",
+        nutrients: ["Protein","Omega 3 acids","Vitamin A"],
+        food: ["Cerelas","Millets","Oats"]
+        },
+        
+        {
+        id: 15,
+        name: "Ascariasis",
+        nutrients: ["Zinc","Iron","Vitamin A"],
+        food: ["Brown Rice","Leafy Greens","Nuts"]
+        },
+        
+        {
+        id: 16,
+        name: "Osteoporosis",
+        nutrients: ["Calcium","Vitamin K","Magnesium"],
+        food: ["Spinach","Beans","Cabbage"]
+        },
+        
+        {
+        id: 17,
+        name: "Lung Cancer",
+        nutrients: ["Protein","Vitamin D","Antioxidants"],
+        food: ["Salman","Carrots"]
+        },
+        
+        {
+        id: 18,
+        name: "Breast Cancer",
+        nutrients: ["Iron","Vitamin D","Calcium"],
+        food: ["Fatty Fish","Red meat","Dates"]
+        },
+        
+        {
+        id: 19,
+        name: "Brain Tumor",
+        nutrients: ["Vitamin B","Vitamin D"],
+        food: ["Brocoli","Oats","Whole Grains"]
+        },
+        
+        {
+        id: 20,
+        name: "Neuro Muscular Disorder",
+        nutrients: ["Zinc","Fiber","Glucosamine"],
+        food: ["Cereals","Brown Rice","Berries","Dairy"]
+        },
+        
+        {
+        id: 21,
+        name: "Inflammatory Bowel Disease",
+        nutrients: ["Vitamin D","Low Fat","Fiber"],
+        food: ["Eggs","Bananas","Fish","Apple"]
+        },
+        
+        {
+        id: 22,
+        name: "Lupus",
+        nutrients: ["Vitamin B6","Vitamin B12","Protein"],
+        food: ["Beans","Nuts","Eggs","Sprouts"]
+        },
+        
+        {
+        id: 23,
+        name: "Stroke",
+        nutrients: ["Folate","Vitamin E"],
+        food: ["Rice","Barley","Bread"]
+        },
+        
+        {
+        id: 24,
+        name: "Alzheimer's Disease",
+        nutrients: ["Vitamin B","Chlorine"],
+        food: ["Fish","Whole Grains","Poultry"]
+        },
+        
+        {
+        id: 25,
+        name: "Coronary Artery Disease",
+        nutrients: ["Omega 3 Fatty Acids","Vitamin B3"],
+        food: ["Egg","Bread","Whole Grains"]
+        },
+        
+        {
+        id: 26,
+        name: "Bronchitis",
+        nutrients: ["Citrus","Proteins"],
+        food: ["Pumpkin","Fish","Berries","Lemon"]
+        },
+        
+        {
+        id: 27,
+        name: "Respiratory Infection",
+        nutrients: ["Vitamin C","Vitamin D"],
+        food: ["Orange","Sweet Lime","Red meat"]
+        },
+        
+        {
+        id: 28,
+        name: "PCOD/PCOS",
+        nutrients: ["Omega Fatty Acid","Vitamin D"],
+        food: ["Salmon","Spinach","Red meat","Brocoli"]
+        },
+        
+        {
+        id: 29,
+        name: "Appendicitis",
+        nutrients: ["Vitamin A","Vitamin C","Zinc"],
+        food: ["Whole Grains","Greens","Carrots"]
+        },
+        
+        {
+        id: 30,
+        name: "Tonsillitis",
+        nutrients: ["Carotenoids","Proteins"],
+        food: ["Egg","Pomegranate Juice","Yogurts"]
+        }
+        
+        
 ]
