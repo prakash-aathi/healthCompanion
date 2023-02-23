8edc4b40c1ffd9a82e3578593c658364b14803d1
Prakash A <prakashaathi321@gmail.com>
2023-02-23 11:30:12 +0530

    mm

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <div className='bg-blue-500 text-center text-white px-4 py-2'>
-        <h2 className='text-2xl font-semibold'>Health Companion</h2>
+        <h2 className='text-2xl font-semibold'>HealthCare Companion</h2>
     </div>
   )
 }
